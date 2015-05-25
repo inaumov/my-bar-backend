a09af4ce84f418f154adff49415cde2bd398c215
unknown <ina@PC-ina.kyiv.ciklum.net>
2015-05-25 16:31:40 +0300

    Removed JSF dependencies and view files

--- mybar/src/main/java/mybar/app/impl/AuthenticationServiceImpl.java
+++ mybar/src/main/java/mybar/app/impl/AuthenticationServiceImpl.java
@@ -1,15 +1,14 @@
 package mybar.app.impl;
 
+import mybar.app.AuthenticationService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import mybar.app.AuthenticationService;
 
 import javax.annotation.Resource;
-import javax.faces.context.FacesContext;
 
 @Service("authenticationService")
 public class AuthenticationServiceImpl implements AuthenticationService {
@@ -38,6 +37,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public void logout() {
         SecurityContextHolder.getContext().setAuthentication(null);
-        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+        // TODO: invalidate session;
     }
 }
