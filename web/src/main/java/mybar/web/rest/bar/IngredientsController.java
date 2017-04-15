8cdf7d4853fe57121a1dc250077f3d43a8629459
Igor Naumov <naumov@live.com>
2017-04-15 21:15:00 +0300

    Simplify filtering of ingredients by type in controller

--- web/src/main/java/mybar/web/rest/bar/IngredientsController.java
+++ web/src/main/java/mybar/web/rest/bar/IngredientsController.java
@@ -1,18 +1,15 @@
 package mybar.web.rest.bar;
 
 import com.google.common.base.Function;
-import com.google.common.base.Predicate;
 import com.google.common.base.Strings;
-import com.google.common.collect.Collections2;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import mybar.api.bar.ingredient.IAdditive;
 import mybar.api.bar.ingredient.IBeverage;
 import mybar.api.bar.ingredient.IDrink;
 import mybar.api.bar.ingredient.IIngredient;
 import mybar.app.RestBeanConverter;
-import mybar.app.bean.bar.ingredient.AdditiveBean;
-import mybar.app.bean.bar.ingredient.BeverageBean;
-import mybar.app.bean.bar.ingredient.DrinkBean;
 import mybar.service.bar.IngredientService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,9 +21,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -57,68 +51,50 @@ public class IngredientsController {
             return new ResponseEntity<>(Collections.emptyList(), HttpStatus.OK);
         }
 
-        Collection beverages = filter(ingredients, IBeverage.class);
-        Collection drinks = filter(ingredients, IDrink.class);
-        Collection additives = filter(ingredients, IAdditive.class);
+        Iterable<IBeverage> beverages = Iterables.filter(ingredients, IBeverage.class);
+        Iterable<IDrink> drinks = Iterables.filter(ingredients, IDrink.class);
+        Iterable<IAdditive> additives = Iterables.filter(ingredients, IAdditive.class);
 
-        ImmutableMap.Builder<String, Collection<IIngredient>> builder = ImmutableMap.builder();
-        putIfPresent(builder, IBeverage.GROUP_NAME, new IngredientsMapper<>(BeverageBean.class).map(beverages));
-        putIfPresent(builder, IDrink.GROUP_NAME, new IngredientsMapper<>(DrinkBean.class).map(drinks));
-        putIfPresent(builder, IAdditive.GROUP_NAME, new IngredientsMapper<>(AdditiveBean.class).map(additives));
+        ImmutableMap.Builder<String, Iterable<IIngredient>> builder = ImmutableMap.builder();
+        putIfPresent(builder, IBeverage.GROUP_NAME, beverages);
+        putIfPresent(builder, IDrink.GROUP_NAME, drinks);
+        putIfPresent(builder, IAdditive.GROUP_NAME, additives);
 
-        ImmutableMap<String, Collection<IIngredient>> responseMap = builder.build();
+        ImmutableMap<String, Iterable<IIngredient>> responseMap = builder.build();
         if (responseMap.size() == 1) {
             return new ResponseEntity<>(responseMap.get(groupNameParam), HttpStatus.OK);
         }
         return new ResponseEntity<>(responseMap, HttpStatus.OK);
     }
 
-    private void putIfPresent(ImmutableMap.Builder<String, Collection<IIngredient>> builder, String groupName, Collection beans) {
-        if (beans.isEmpty()) {
+    private void putIfPresent(ImmutableMap.Builder<String, Iterable<IIngredient>> builder, String groupName, Iterable<? extends IIngredient> filtered) {
+        if (!filtered.iterator().hasNext()) {
             return;
         }
-        builder.put(groupName, beans);
+        builder.put(groupName, Lists.newArrayList(Iterables.transform(filtered, ingredientFunction())));
     }
 
-    private <T extends IIngredient> Collection<T> filter(List<T> all, final Class c) {
-        return Collections2.filter(all, new Predicate<T>() {
+    private static <DTO, BEAN extends IIngredient> Function<DTO, BEAN> ingredientFunction() {
+        return new Function<DTO, BEAN>() {
+
             @Override
-            public boolean apply(T t) {
-                return c.isInstance(t);
-            }
-        });
-    }
-
-    private class IngredientsMapper<OUT> {
-
-        private final Class<OUT> type;
-
-        private IngredientsMapper() {
-            Type t = this.getClass().getGenericSuperclass();
-            ParameterizedType pt = (ParameterizedType) t;
-            type = (Class) pt.getActualTypeArguments()[0];
-        }
-
-        private IngredientsMapper(Class<OUT> type) {
-            this.type = type;
-        }
-
-        private <IN extends IIngredient> Collection<OUT> map(Collection<IN> filtered) {
-            return Collections2.transform(filtered, new Function<IN, OUT>() {
-                @Override
-                public OUT apply(IN input) {
-                    if (type == AdditiveBean.class) {
-                        return (OUT) RestBeanConverter.from((IAdditive) input);
-                    } else if (type == DrinkBean.class) {
-                        return (OUT) RestBeanConverter.from((IDrink) input);
-                    } else if (type == BeverageBean.class) {
-                        return (OUT) RestBeanConverter.from((IBeverage) input);
-                    }
-                    return null;
+            public BEAN apply(DTO input) {
+                IIngredient from = null;
+                if (input instanceof IAdditive) {
+                    from = RestBeanConverter.from((IAdditive) input);
+                } else if (input instanceof IDrink) {
+                    from = RestBeanConverter.from((IDrink) input);
+                } else if (input instanceof IBeverage) {
+                    from = RestBeanConverter.from((IBeverage) input);
                 }
-            });
+                return uncheckedCast(from);
+            }
 
-        }
+            @SuppressWarnings({"unchecked"})
+            private BEAN uncheckedCast(Object obj) {
+                return (BEAN) obj;
+            }
+        };
     }
 
 }
