1506f1115860e209023f3d811c853c5bdd348152
Igor Naumov <naumov@live.com>
2021-02-09 23:15:20 +0200

    Implement refresh token for app client

--- rest/src/main/java/mybar/web/config/auth/AuthorizationServerConfiguration.java
+++ rest/src/main/java/mybar/web/config/auth/AuthorizationServerConfiguration.java
@@ -4,11 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -18,13 +21,15 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
     private AuthenticationManager authenticationManager;
+    private UserDetailsService userDetailsService;
 
     @Value("signing-key:some-pretty-key")
     private String signingKey;
 
     @Autowired
-    public AuthorizationServerConfiguration(AuthenticationManager authenticationManager) {
+    public AuthorizationServerConfiguration(AuthenticationManager authenticationManager, UserDetailsService userDetailsService) {
         this.authenticationManager = authenticationManager;
+        this.userDetailsService = userDetailsService;
     }
 
     public AuthorizationServerConfiguration() {
@@ -45,6 +50,15 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
         return new JwtTokenStore(accessTokenConverter());
     }
 
+    @Bean
+    @Primary
+    public DefaultTokenServices defaultTokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        defaultTokenServices.setSupportRefreshToken(true);
+        return defaultTokenServices;
+    }
+
     // config
 
     @Override
@@ -52,22 +66,26 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
         endpoints
                 .tokenStore(tokenStore())
                 .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService)
                 .accessTokenConverter(accessTokenConverter());
     }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("api-test")
+                // 1.
+                .withClient("api-tests")
                 .secret("bGl2ZS10ZXN0")
                 .authorizedGrantTypes("password")
                 .scopes("my-bar")
                 .autoApprove("my-bar")
                 .accessTokenValiditySeconds(3600)
+                // 2.
                 .and()
                 .withClient("my-bar-app")
                 .secret("secret001")
-                .authorizedGrantTypes("password")
+                .authorizedGrantTypes("password", "refresh_token")
+                .refreshTokenValiditySeconds(3600 * 24)
                 .scopes("my-bar")
                 .autoApprove("my-bar")
                 .accessTokenValiditySeconds(3600);
