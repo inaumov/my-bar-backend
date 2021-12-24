7f08fb8c508f0dbc51684a93c8076f8fcc5edcea
Igor Naumov <naumov@live.com>
2021-12-24 13:54:21 +0100

    Read test data from kafka tc after normal event is sent

--- kafka-events/src/test/java/common/events/producer/TestEvent.java
+++ kafka-events/src/test/java/common/events/producer/TestEvent.java
@@ -1,12 +1,10 @@
 package common.events.producer;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class TestEvent {
