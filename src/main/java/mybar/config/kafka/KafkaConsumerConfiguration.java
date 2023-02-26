4538e55595492bb5163618a814d6518ff48dae29
Igor Naumov <naumov@live.com>
2023-02-26 20:30:12 +0200

    Kafka configuration review

---
+++ src/main/java/mybar/config/kafka/KafkaConsumerConfiguration.java
@@ -0,0 +1,10 @@
+package mybar.config.kafka;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@EnableKafka
+@Configuration
+public class KafkaConsumerConfiguration {
+
+}
